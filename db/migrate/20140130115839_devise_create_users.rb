3f12ac647535f5babd7de7625f1d261d19094ebf
wieczorek1990 <wieczorek1990@gmail.com>
2014-01-31 18:58:23 +0100

    admin user

--- db/migrate/20140130115839_devise_create_users.rb
+++ db/migrate/20140130115839_devise_create_users.rb
@@ -38,11 +38,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
-    add_admin
-  end
-
-  def add_admin
-    admin = User.new(email: 'admin@blog.pl', password: 'password').save
-    admin.add_role :admin
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'admin@blog.pl', password: 'password').add_role :admin
