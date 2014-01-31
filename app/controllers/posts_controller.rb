cefc24ded02a755d8de1917d6bb2dc3071e9f62a
wieczorek1990 <wieczorek1990@gmail.com>
2014-01-31 18:28:45 +0100

    get rid of facebook

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < InheritedResources::Base
-  before_filter :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   def permitted_params
     params.permit(post: [:title, :content])
   end
